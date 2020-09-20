1805bdb3d76831c90d4ef8ae74be001540e26ddd
asasinder <kulandre9@gmail.com>
2020-09-20 12:59:09 +0300

    first commit

---
+++ lesson3/functions.py
@@ -0,0 +1,12 @@
+def hello():
+    print('hello')
+
+def hello2(name,age):
+    year = 2020 - age
+    print('Hi', name, 'Your year of birth:', year)
+    print('hi '+name, 'Your year of birth:', year)
+
+
+hello()
+
+hello2('Andrii', 15)
